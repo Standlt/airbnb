47183aaca568da3bd00aadd8a89c4cd3f6551c94
Standlt dlt <estanislao.delatorregarcia@kellogg.northwestern.edu>
2024-01-14 17:25:13 +0000

    First batch (1-3) of homework exercises

--- practice-3.sql
+++ practice-3.sql
@@ -1,5 +1,14 @@
 -- What are all the neighborhoods, alphabetically?
 
+select neighborhood from listings
+group by neighborhood;
+
+-- select * from listings
+-- group by neighborhood;
+
+-- select id, neighborhood from listings
+-- group by neighborhood;
+
 -- +------------------------+
 -- | Albany Park            |
 -- | Archer Heights         |
